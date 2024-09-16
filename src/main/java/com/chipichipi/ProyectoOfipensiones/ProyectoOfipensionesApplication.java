5356c09747568bc22b4b21393314a3e12d046839
TomasSiSa18 <t.sierras@uniandes.edu.co>
2024-09-16 10:24:32 -0500

    Commit Repo con Spring

---
+++ src/main/java/com/chipichipi/ProyectoOfipensiones/ProyectoOfipensionesApplication.java
@@ -0,0 +1,13 @@
+package com.chipichipi.ProyectoOfipensiones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoOfipensionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoOfipensionesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chipichipi/ProyectoOfipensiones/ProyectoOfipensionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chipichipi.ProyectoOfipensiones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoOfipensionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
