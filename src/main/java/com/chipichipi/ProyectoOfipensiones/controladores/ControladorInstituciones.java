847c72814b710cb19117950976f2504631f914be
TomasSiSa18 <t.sierras@uniandes.edu.co>
2024-09-18 21:42:59 -0500

    Cambio

--- src/main/java/com/chipichipi/ProyectoOfipensiones/controladores/ControladorInstituciones.java
+++ src/main/java/com/chipichipi/ProyectoOfipensiones/controladores/ControladorInstituciones.java
@@ -29,7 +29,7 @@ public class ControladorInstituciones {
 
     @PostMapping("/new")
     @ResponseStatus(code = HttpStatus.CREATED)
-    public Institucion crearInstitucion(Institucion institucion) {
+    public Institucion crearInstitucion(@RequestBody Institucion institucion) {
         Institucion institucionNueva = institucionServicio.crearInstitucion(institucion);
         return institucionNueva;
     }
