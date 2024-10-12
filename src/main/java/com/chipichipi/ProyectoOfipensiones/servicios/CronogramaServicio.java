b2aecb1d1e2fd2f8d08fb4a48a2385718bfa1454
TomasSiSa18 <t.sierras@uniandes.edu.co>
2024-10-12 14:35:52 -0500

    Avance Final

---
+++ src/main/java/com/chipichipi/ProyectoOfipensiones/servicios/CronogramaServicio.java
@@ -0,0 +1,15 @@
+package com.chipichipi.ProyectoOfipensiones.servicios;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CronogramaServicio {
+
+    public Boolean cronogramaValido(int[] c) {
+        return true;
+    }
+    
+ 
+
+
+}
