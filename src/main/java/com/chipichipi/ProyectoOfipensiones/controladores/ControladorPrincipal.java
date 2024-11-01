a0ed5f0a335402842066a6d01dec0672fbb7d73f
TomasSiSa18 <t.sierras@uniandes.edu.co>
2024-10-31 23:25:46 -0500

    Auth0

--- src/main/java/com/chipichipi/ProyectoOfipensiones/controladores/ControladorPrincipal.java
+++ src/main/java/com/chipichipi/ProyectoOfipensiones/controladores/ControladorPrincipal.java
@@ -1,18 +1,27 @@
 package com.chipichipi.ProyectoOfipensiones.controladores;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import ch.qos.logback.core.model.Model;
+
 @Controller
 public class ControladorPrincipal {
 
     @RequestMapping("/")
-    public String index() {
+    public String index(Model model, @AuthenticationPrincipal OidcUser principal) {
         return "index";
+    }
+
+    @RequestMapping("/login/oauth2/code/okta")
+    public String login() {
+        return "login";
     }
 
     @GetMapping("/health")
