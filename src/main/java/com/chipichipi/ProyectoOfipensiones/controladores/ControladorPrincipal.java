08bb87fd74cad5fd41b68f84ed0c2252ebbc1334
TomasSiSa18 <t.sierras@uniandes.edu.co>
2024-11-05 20:38:16 -0500

    Habemos Correo

--- src/main/java/com/chipichipi/ProyectoOfipensiones/controladores/ControladorPrincipal.java
+++ src/main/java/com/chipichipi/ProyectoOfipensiones/controladores/ControladorPrincipal.java
@@ -1,5 +1,6 @@
 package com.chipichipi.ProyectoOfipensiones.controladores;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +8,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import com.chipichipi.ProyectoOfipensiones.servicios.SendEmailService;
+
 @Controller
 public class ControladorPrincipal {
+
+    @Autowired
+    private SendEmailService sendEmailService;
 
     @RequestMapping("/")
     public String index() {
@@ -20,6 +26,15 @@ public class ControladorPrincipal {
     @ResponseBody
     public String health() {
         return "OK";
+    }
+
+    @RequestMapping("/mandarCorreo")
+    public String mandarCorreo() {
+
+        sendEmailService.sendEmail("sierratomy@gmail.com", "Test", "Test Email");
+
+        return "index";
+
     }
     
     
