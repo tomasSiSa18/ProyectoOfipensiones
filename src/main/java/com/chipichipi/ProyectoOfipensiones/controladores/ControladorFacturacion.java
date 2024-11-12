f6150cb7612e5674e9c49046179e9737e22db6f8
andres <a.charryc@uniandes.edu.co>
2024-11-12 11:17:08 -0500

    arreglos

--- src/main/java/com/chipichipi/ProyectoOfipensiones/controladores/ControladorFacturacion.java
+++ src/main/java/com/chipichipi/ProyectoOfipensiones/controladores/ControladorFacturacion.java
@@ -1,6 +1,6 @@
 package com.chipichipi.ProyectoOfipensiones.controladores;
 
-import java.util.Collection;
+
 
 import org.springframework.ui.Model;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.chipichipi.ProyectoOfipensiones.modelo.Factura;
 import com.chipichipi.ProyectoOfipensiones.servicios.FacturaServicio;
 
 @Controller
--- src/main/java/com/chipichipi/ProyectoOfipensiones/servicios/UsuariosSerivicio.java
+++ src/main/java/com/chipichipi/ProyectoOfipensiones/servicios/UsuariosSerivicio.java
@@ -1,27 +1,12 @@
 package com.chipichipi.ProyectoOfipensiones.servicios;
 
-import java.util.ArrayList;
-import java.util.Collection;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import com.chipichipi.ProyectoOfipensiones.modelo.Estudiante;
-import com.chipichipi.ProyectoOfipensiones.modelo.Factura;
-import com.chipichipi.ProyectoOfipensiones.modelo.ResponsableEconomico;
-import com.chipichipi.ProyectoOfipensiones.repositorios.UsuariosRepository;
-
-import org.bson.types.ObjectId;
 
 @Service
 public class UsuariosSerivicio {
 
-
-    @Autowired
-    private FacturaServicio facturaServicio;
-    @Autowired
-    private UsuariosRepository usuarioRepository;
 
 
     public boolean existeUsuario(int id) {
