5c3c9dd9b314538c9f2e9bbeadc680cc4accab7d
andres <a.charryc@uniandes.edu.co>
2024-11-26 07:19:38 -0500

    Firts commit

--- src/main/java/com/chipichipi/ProyectoOfipensiones/controladores/ControladorPrincipal.java
+++ src/main/java/com/chipichipi/ProyectoOfipensiones/controladores/ControladorPrincipal.java
@@ -11,14 +11,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import com.chipichipi.ProyectoOfipensiones.modelo.Usuario;
 import com.chipichipi.ProyectoOfipensiones.servicios.UsuariosSerivicio;
 
 import org.springframework.ui.Model;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+import com.chipichipi.ProyectoOfipensiones.config.AppConfig;
 
 @Controller
 public class ControladorPrincipal {
     @Autowired
     UsuariosSerivicio usuariosSerivicio;
+    @Autowired RestTemplate restTemplate;
 
     @RequestMapping("/")
 public String index(Model model, @AuthenticationPrincipal OidcUser principal) {
@@ -34,7 +40,10 @@ public String home(Model model, @AuthenticationPrincipal OidcUser principal) {
 
         model.addAttribute("rol", rol);
         model.addAttribute("id", id);
-        
+        String microservicioUrl = "http://localhost:8081/api/usuarios/" + id;
+        Usuario usuario = restTemplate.getForObject(microservicioUrl, Usuario.class);
+        System.out.println(usuario.getId());
+        System.out.println(usuario.getNombre());
         return "home";
     }
     return "index";
